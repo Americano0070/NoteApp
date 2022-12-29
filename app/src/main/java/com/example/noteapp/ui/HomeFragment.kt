6f30461aced43b57e4d25b0569904ca6b985f63d
Americano <americanboy0070@gmail.com>
2022-12-29 19:38:07 +0500

    Deadline 13-December

--- app/src/main/java/com/example/noteapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/HomeFragment.kt
@@ -2,19 +2,21 @@ package com.example.noteapp.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private val binding: FragmentHomeBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+
+
+        }
     }
 
 }
--- app/src/main/java/com/example/noteapp/ui/UpdateFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/UpdateFragment.kt
@@ -2,20 +2,20 @@ package com.example.noteapp.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentUpdateBinding
 
 
-class UpdateFragment : Fragment() {
+class UpdateFragment : Fragment(R.layout.fragment_update) {
+    private val binding: FragmentUpdateBinding by viewBinding()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+
+        }
     }
 
 }
