c013b7e65189c96e94f09ee5e327823b2cad486a
Americano <americanboy0070@gmail.com>
2022-12-29 19:38:07 +0500

    Deadline 13-December

--- app/src/main/java/com/example/noteapp/ui/AddFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/AddFragment.kt
@@ -2,20 +2,21 @@ package com.example.noteapp.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentAddBinding
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(R.layout.fragment_add) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+    private val binding: FragmentAddBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+
+
+        }
     }
-
 
 }
