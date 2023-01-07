7ce6123da99171ff0ad8095822115af8ed0fb439
Americano <americanboy0070@gmail.com>
2023-01-07 19:32:29 +0500

    Merge remote-tracking branch 'github/master'

---
+++ app/src/main/java/com/example/noteapp/dataBase/ToDoData.kt
@@ -0,0 +1,13 @@
+package com.example.noteapp.dataBase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var tittle: String,
+    var priority: Priority,
+    var description: String
+)
--- app/src/main/java/com/example/noteapp/dataBase/ToDoDataBase.kt
+++ app/src/main/java/com/example/noteapp/dataBase/ToDoDataBase.kt
@@ -1,13 +1,36 @@
 package com.example.noteapp.dataBase
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-@Entity(tableName = "todo_table")
-data class ToDoDataBase(
-    @PrimaryKey(autoGenerate = true)
-    var id: Int,
-    var tittle: String,
-    var priority: Priority,
-    var description: String
-)
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+abstract class ToDoDataBase : RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ToDoDataBase? = null
+
+        fun getDataBase(context: Context): ToDoDataBase {
+            val tempInstanse = INSTANCE
+            if (tempInstanse != null) {
+                return tempInstanse
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ToDoDataBase::class.java,
+                    "todo_database"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+
+}
